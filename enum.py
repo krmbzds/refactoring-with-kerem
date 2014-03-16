5ed7e1b957d4ea6251dcc4040e32b6f614658d68
Kerem Bozdas <krmbzds.github@gmail.com>
2014-03-16 22:24:18 +0200

    Remove redeclared enum function

--- enum.py
+++ enum.py
@@ -1,7 +1,3 @@
-def enum(**enums):
-    return type('Enum', (), enums)
-
-
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
     reverse = dict((value, key) for key, value in enums.iteritems())
