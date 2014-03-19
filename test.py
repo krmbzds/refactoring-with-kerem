60b0fc16d02b9d233deade5deab776d29bdc3198
Kerem Bozdas <krmbzds.github@gmail.com>
2014-03-19 22:50:03 +0200

    Update test file

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from Visualizer import *
 
 g = Grid.get_random_grid()
-#r = Robby.get_random_robby()
-r = Robby.get_robby_from_file('generations/generation-1-robot-best.txt')
+r = Robby.get_random_robby()
+#r = Robby.get_robby_from_file('generations/generation-1-robot-best.txt')
 g.set_robby(r)
 r.set_grid(g)
 
